e6ee2b2fe77b4abcf830586e7bccef009e4e658e
Joeyanatasa <115853239+joeyanatasa@users.noreply.github.com>
2022-10-22 13:29:27 +0800

    Add files via upload

---
+++ anjing.py
@@ -0,0 +1,5 @@
+import telepot
+
+bot = telepot.Bot("5622315128:AAGG6TPO7djGbzEF8j-UCZ27yUmltQpynQs")
+
+bot.sendPhoto(chat_id="1274491768", photo=("https://media.vogue.co.uk/photos/6009a758b46d627b9d883285/2:3/w_2560%2Cc_limit/001-Olivia-Rodrigo-vogue-200121-creditCredit-ERICA-HERNA%25CC%2581NDEZ.jpg"))
